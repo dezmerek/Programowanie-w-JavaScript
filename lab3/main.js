482df4599801a6aa9e90a1e031dbc2735e568cb7
dezmerek <mszymanski027@gmail.com>
2022-10-22 16:19:45 +0200

    dodanie opcji nagrywania

--- lab3/main.js
+++ lab3/main.js
@@ -1,6 +1,21 @@
 document.addEventListener("keypress", nacisniecieKlawisza);
 
+//pobieranie z html
+const przyciskRozpoczeciaNagrywania = document.querySelector("#startNagrywanie");
+const przyciskZakonczeniaNagrywania = document.querySelector("#stopNagrywanie");
+const przyciskOdtwarzaniaNagrania = document.querySelector("#playOdtwarzanie");
+
+//zmienne przechowujace
+let czasRozpoczeciaNagrywania;
+let pierwszeNagranie = [];
+let aktualneNagranie = [];
+
+przyciskRozpoczeciaNagrywania.addEventListener("click", rozpocznijNagrywanie);
+przyciskZakonczeniaNagrywania.addEventListener("click", zakonczNagrywanie);
+przyciskOdtwarzaniaNagrania.addEventListener("click", odtworzNagranie);
+
 function nacisniecieKlawisza(zdarzenie) {
+  let dzwiek;
   switch (zdarzenie.key) {
     case "1":
       dzwiek = "boom";
@@ -13,7 +28,7 @@ function nacisniecieKlawisza(zdarzenie) {
       break;
     case "4":
       dzwiek = "kick";
-      break;a
+      break;
     case "5":
       dzwiek = "openhat";
       break;
@@ -32,11 +47,57 @@ function nacisniecieKlawisza(zdarzenie) {
     default:
       return;
   }
+
   odtworzDzwiek(dzwiek);
 }
 
 function odtworzDzwiek(dzwiek) {
+  if (czyNagrywa) {
+    nagrajDzwiek(dzwiek);
+  }
   const audioTag = document.querySelector(`#${dzwiek}`);
   audioTag.currentTime = 0;
   audioTag.play();
+}
+
+let czyNagrywa = false;
+
+function rozpocznijNagrywanie() {
+  if (this.id === "startNagrywanie") {
+    pierwszeNagranie = [];
+    aktualneNagranie = pierwszeNagranie;
+  } else {
+    console.log("error");
+  }
+  console.log("Nagrywanie!");
+  czyNagrywa = true;
+  czasRozpoczeciaNagrywania = Date.now();
+}
+
+function zakonczNagrywanie() {
+  console.log("Zakończono nagrywanie!");
+  czyNagrywa = false;
+}
+
+function odtworzNagranie() {
+  if (this.id === "playOdtwarzanie") {
+    aktualneNagranie = pierwszeNagranie;
+    console.log("Odtwarzanie!");
+  } else {
+    console.log("error");
+  }
+
+  if (aktualneNagranie.length === 0) return;
+  aktualneNagranie.forEach((nagranie) => {
+    setTimeout(() => {
+      odtworzDzwiek(nagranie.klucz);
+    }, nagranie.czasRozpoczecia);
+  });
+}
+
+function nagrajDzwiek(dzwiek) {
+  aktualneNagranie.push({
+    klucz: dzwiek,
+    czasRozpoczecia: Date.now() - czasRozpoczeciaNagrywania,
+  });
 }
