29b18c523e2a6995a385d5aaec654d2a6fd2294d
dezmerek <mszymanski027@gmail.com>
2022-12-28 12:38:18 +0100

    poprawki

--- lab6/main.js
+++ lab6/main.js
@@ -1,16 +1,16 @@
 const area = document.getElementById("area");
 const ball = document.getElementById("ball");
 const holeContainer = document.getElementById("holeContainer");
-const timer = document.getElementById("timer");
-
+const timeTakenElement = document.getElementById("timeTaken");
 setInterval(() => {
-  timer.innerHTML = (timeElapsed / 1000).toFixed(2) + " seconds";
+  timer.innerHTML = (timeElapsed / 1000).toFixed(2) + "s";
 }, 100);
-
 let x = 0;
 let y = 0;
 let startTime;
+let endTime;
 let timeElapsed = 0;
+let holesFilled = 0;
 let intervalId;
 
 area.style.width = "400px";
@@ -34,10 +34,8 @@ window.addEventListener("deviceorientation", (event) => {
       timeElapsed = Date.now() - startTime;
     }, 1000);
   }
-
   x += event.gamma / 10;
   y += event.beta / 10;
-
   x = Math.max(0, Math.min(x, areaWidth - ball.offsetWidth));
   y = Math.max(0, Math.min(y, areaHeight - ball.offsetHeight));
 });
@@ -54,25 +52,30 @@ function animate() {
       y < hole.offsetTop + hole.offsetHeight
     ) {
       hole.style.display = "none";
+      holesFilled++;
+      isHoleFilled = true;
+      if (holesFilled === holes.length) {
+        endTime = Date.now();
+        const timeTaken = (endTime - startTime) / 1000;
+        timeTakenElement.innerHTML = `Końcowy czas: ${
+          timeElapsed / 1000
+        } seconds`;
+        clearInterval(intervalId);
+      }
     }
   }
-
   requestAnimationFrame(animate);
 }
 
 function randomHolePositions() {
   for (const hole of holes) {
     hole.style.display = "block";
-    hole.style.left = Math.floor(Math.random() * (areaWidth - hole.offsetWidth)) + "px";
-    hole.style.top = Math.floor(Math.random() * (areaHeight - hole.offsetHeight)) + "px";
+    hole.style.left =
+      Math.floor(Math.random() * (areaWidth - hole.offsetWidth)) + "px";
+    hole.style.top =
+      Math.floor(Math.random() * (areaHeight - hole.offsetHeight)) + "px";
   }
-  for (const hole of holes) {
-  hole.addEventListener("mouseover", () => {
-    hole.style.display = "none";
-  });
 }
 
-}
 randomHolePositions();
-
 animate();
